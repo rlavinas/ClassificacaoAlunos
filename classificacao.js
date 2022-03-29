c8565326ae2d33ccc355d86e938abea1da029bb7
rafaellaivnas <rafael.lavinas@korusconsultoria.com.br>
2022-03-29 19:36:04 -0300

    Ultimas alteracoes - Final da Aula

--- classificacao.js
+++ classificacao.js
@@ -1,5 +1,43 @@
+// PARTE 1 DO EXERCICIO
+/*
 let quantidadeAlunos = 5;
 
-for (let contador = 0; contador < quantidadeAlunos.length; contador++) {
-    
+for (let contador = 0; contador < listaAlunos.length; contador++) {
+    console.log(contador);
+
+    if (contador == 0) {
+        console.log("O número é ZERO")
+    } else if ((contador % 2) == 0) {
+        console.log("O número é PAR")
+    } else if ((contador % 2) == 1) {
+        console.log("O número é IMPAR")
+    }
+}
+*/
+
+// PARTE 2 DO EXERCICIO
+/*
+let listaAlunos = ["Leonardo", "Robson", "Rafael", "Raiane"];
+
+let quantidadeAlunos = 5;
+
+for (let contador = 0; contador < listaAlunos.length; contador++) {
+    console.log(contador);
+
+    if (contador == 0) {
+        console.log("O número é ZERO")
+    } else if ((contador % 2) == 0) {
+        console.log(`O aluno ${listaAlunos[contador]} é o número ${contador} PAR`);
+    } else if ((contador % 2) == 1) {
+        console.log(`O aluno ${listaAlunos[contador]} é o número ${contador} IMPAR`);
+    }
+}
+*/
+
+// PARTE 3 DO EXERCICIO
+let respostaUsuario = "Sim"
+
+while (respostaUsuario == "Sim"){
+    console.log("Boa noite!");
+    respostaUsuario = "Não";
 }
